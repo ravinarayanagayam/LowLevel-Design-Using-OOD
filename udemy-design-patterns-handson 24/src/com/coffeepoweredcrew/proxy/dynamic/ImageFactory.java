01119d86a7b3e4ed9a7fe124dd892b5a7c7b90f4
Ravi Gayam <ravinarayanagayam@gmail.com>
2024-11-12 23:45:34 -0500

    OOD

---
+++ udemy-design-patterns-handson 24/src/com/coffeepoweredcrew/proxy/dynamic/ImageFactory.java
@@ -0,0 +1,11 @@
+package com.coffeepoweredcrew.proxy.dynamic;
+
+import java.lang.reflect.Proxy;
+
+import com.coffeepoweredcrew.proxy.Image;
+
+//Factory to get image objects. 
+public class ImageFactory {
+	//We'll provide proxy to caller instead of real object
+	
+}
---
+++ udemy-design-patterns-handson 24/src/com/coffeepoweredcrew/proxy/dynamic/ImageInvocationHandler.java
@@ -0,0 +1,6 @@
+package com.coffeepoweredcrew.proxy.dynamic;
+
+//Implement invocation handler. Your "proxy" code goes here.
+public class ImageInvocationHandler {
+	
+}
